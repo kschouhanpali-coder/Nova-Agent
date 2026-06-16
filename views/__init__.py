fc8928ee3738c9cfd14e7b18f27929202fe689d2
Kritesh Singh Chouhan <281302643+kschouhanpali-coder@users.noreply.github.com>
2026-06-16 10:29:51 +0530

    Delete views directory

--- views/__init__.py
+++
@@ -1,5 +0,0 @@
-from views.home import render_home
-from views.hub import render_hub
-from views.chat import render_chat
-from views.settings import render_settings
-from views.sidebar import render_sidebar
